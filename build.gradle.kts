7f19b27f2d0a239c3043c46b0e889d552e804df8
Aliorpse <Aliorpse@qq.com>
2025-07-13 04:17:26 +0800

    docs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,10 @@
 plugins {
     kotlin("jvm") version "2.1.21"
-    id("com.vanniktech.maven.publish") version "0.33.0"
     id("org.jetbrains.dokka") version "2.0.0"
+    id("com.vanniktech.maven.publish") version "0.33.0"
 }
 
 group = "tech.aliorpse.mcutils"
-version = "0.1.3"
 
 repositories {
     mavenCentral()
@@ -18,54 +17,48 @@ dependencies {
     testImplementation(kotlin("test"))
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
-
-tasks.dokkaHtml {
+tasks.dokkaJavadoc {
     outputDirectory.set(layout.buildDirectory.dir("dokka"))
 }
 
-tasks.register<Jar>("javadocJar") {
-    dependsOn(tasks.dokkaHtml)
-    archiveClassifier.set("javadoc")
-    from(layout.buildDirectory.dir("dokka"))
-}
-kotlin {
-    jvmToolchain(21)
+val sourcesJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets["main"].allSource)
 }
 
-java {
-    withSourcesJar()
+val javadocJar by tasks.registering(Jar::class) {
+    dependsOn(tasks.dokkaJavadoc)
+    archiveClassifier.set("javadoc")
+    from(tasks.dokkaJavadoc.flatMap { it.outputDirectory })
 }
 
 configure<com.vanniktech.maven.publish.MavenPublishBaseExtension> {
     publishToMavenCentral()
     signAllPublications()
-    coordinates("tech.aliorpse", "mcutils", "0.1.3")
+    coordinates("tech.aliorpse", "mcutils", "0.1.4")
 
     pom {
-        name = "mcutils"
-        description = "Kotlin library for minecraft operations"
-        url = "https://github.com/Aliorpse/kotlin-mcutils/"
-        inceptionYear = "2025"
+        name.set("mcutils")
+        description.set("Kotlin library for minecraft operations")
+        url.set("https://github.com/Aliorpse/kotlin-mcutils/")
+        inceptionYear.set("2025")
         licenses {
             license {
-                name = "MIT"
-                url = "https://opensource.org/licenses/MIT"
+                name.set("MIT")
+                url.set("https://opensource.org/licenses/MIT")
             }
         }
         developers {
             developer {
-                id = "aliorpse"
-                name = "Aliorpse"
-                url = "https://github.com/Aliorpse/"
+                id.set("aliorpse")
+                name.set("Aliorpse")
+                url.set("https://github.com/Aliorpse/")
             }
         }
         scm {
-            url = "https://github.com/Aliorpse/kotlin-mcutils/"
-            connection = "scm:git:git://github.com/Aliorpse/kotlin-mcutils.git"
-            developerConnection = "scm:git:ssh://git@github.com/Aliorpse/kotlin-mcutils.git"
+            url.set("https://github.com/Aliorpse/kotlin-mcutils/")
+            connection.set("scm:git:git://github.com/Aliorpse/kotlin-mcutils.git")
+            developerConnection.set("scm:git:ssh://git@github.com/Aliorpse/kotlin-mcutils.git")
         }
     }
 }
