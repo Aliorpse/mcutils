761fab6c0c4a1566daa7b1a153e369b65bdb0b7c
Aliorpse <Aliorpse@qq.com>
2025-12-07 08:06:18 +0800

    refactor: move SRV related data classes to internal entity folder

---
+++ src/commonMain/kotlin/tech/aliorpse/mcutils/internal/entity/SrvRecord.kt
@@ -0,0 +1,28 @@
+package tech.aliorpse.mcutils.internal.entity
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+
+internal data class SrvRecord(
+    val target: String,
+    val port: Int,
+    val priority: Int,
+    val weight: Int,
+)
+
+internal data class CachedResult(
+    val expireAt: Long,
+    val records: List<SrvRecord>
+)
+
+@Serializable
+internal data class GoogleDohResponse(
+    @SerialName("Answer") val answer: List<DohAnswer>? = null
+)
+
+@Serializable
+internal data class DohAnswer(
+    val data: String,
+    @SerialName("TTL") val ttl: Int? = null
+)
