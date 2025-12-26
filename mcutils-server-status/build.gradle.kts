7ac3d92bde290d6b9efeda057aadbaa7448a0e5d
Aliorpse <Aliorpse@qq.com>
2025-12-27 07:29:51 +0800

    chore: adjust code indent [skip ci]

--- mcutils-server-status/build.gradle.kts
+++ mcutils-server-status/build.gradle.kts
@@ -11,8 +11,8 @@ kotlin {
             api(project(":core"))
             implementation(project(":util"))
             api(project(":color"))
-            implementation(libs.ktor.network)
-                       implementation(libs.kotlinx.serialization.json)
+             implementation(libs.ktor.network)
+             implementation(libs.kotlinx.serialization.json)
         }
         jvmMain.dependencies {
             implementation(libs.minidns.hla)
