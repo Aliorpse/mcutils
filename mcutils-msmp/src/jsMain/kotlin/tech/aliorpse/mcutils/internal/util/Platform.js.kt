acb75a1613375fde8872be2bfe64d2b622de7645
Aliorpse <Aliorpse@qq.com>
2026-01-31 22:44:23 +0800

    ok.

--- mcutils-msmp/src/jsMain/kotlin/tech/aliorpse/mcutils/internal/util/Platform.js.kt
+++ mcutils-msmp/src/jsMain/kotlin/tech/aliorpse/mcutils/internal/util/Platform.js.kt
@@ -1,4 +1,4 @@
 package tech.aliorpse.mcutils.internal.util
 
 internal actual val isBrowser: Boolean =
-    js("typeof window !== 'undefined' && typeof window.document !== 'undefined'") as Boolean
+    js("typeof window !== 'undefined' && typeof window.document !== 'undefined'")
