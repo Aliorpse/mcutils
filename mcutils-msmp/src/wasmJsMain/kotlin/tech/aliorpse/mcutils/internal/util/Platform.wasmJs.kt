765939928968d58c89c900ac911d381c618f95cd
Aliorpse <Aliorpse@qq.com>
2026-01-31 22:35:26 +0800

    fix

--- mcutils-msmp/src/wasmJsMain/kotlin/tech/aliorpse/mcutils/internal/util/Platform.wasmJs.kt
+++ mcutils-msmp/src/wasmJsMain/kotlin/tech/aliorpse/mcutils/internal/util/Platform.wasmJs.kt
@@ -1,5 +1,5 @@
 package tech.aliorpse.mcutils.internal.util
 
 @OptIn(ExperimentalWasmJsInterop::class)
-internal actual val isBrowser: Boolean =
-    js("typeof window !== 'undefined' && typeof window.document !== 'undefined'") as Boolean
+internal actual val isBrowser: Boolean
+    get() = js("typeof window !== 'undefined' && typeof window.document !== 'undefined'")
